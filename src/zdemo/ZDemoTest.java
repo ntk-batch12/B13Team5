60329fbcf1f3d781087978b19fb9f3665426ba7a
sayed <sayed@Sameer.mynetworksettings.com>
2025-01-31 21:46:21 -0500

    added new variable

--- src/zdemo/ZDemoTest.java
+++ src/zdemo/ZDemoTest.java
@@ -4,6 +4,11 @@ public class ZDemoTest {
 
 	public static void main(String[] args) {
 		System.out.println("Team 5");
-	}
+		
+		System.out.println("United States Of America");
+		  char myChar = 'X'; 
+    //new code
+	        System.out.println("The value of myChar is: " + myChar);
 
+   }
 }
